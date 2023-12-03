8429bf3d977f31957f7af49a73c554c11dbfd77c
Brad Edwards <j.bradley.edwards@gmail.com>
2023-12-02 23:00:07 -0800

    Lint tests

--- services/fetch_daily_summaries/tests/test_lambda_handler.py
+++ services/fetch_daily_summaries/tests/test_lambda_handler.py
@@ -1,24 +1,42 @@
 import os
 import unittest
 from unittest.mock import patch, MagicMock
-from services.fetch_daily_summaries.src.fetch_daily_summaries import lambda_handler
+from services.fetch_daily_summaries.src.fetch_daily_summaries \
+    import lambda_handler
+
 
 class TestLambdaHandler(unittest.TestCase):
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.process_fetch', return_value=True)
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.get_earliest_unfetched_date', return_value='2023-01-01')
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.boto3.client')
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.requests.get')
+    # Define the common base path as a constant
+    BASE_PATH = 'services.fetch_daily_summaries.src.fetch_daily_summaries.'
+
+    # Define specific paths by concatenating with the base path
+    PROCESS_FETCH_PATH = BASE_PATH + 'process_fetch'
+    EARLIEST_UNFETCHED_DATE_PATH = BASE_PATH + 'get_earliest_unfetched_date'
+    BOTO3_CLIENT_PATH = BASE_PATH + 'boto3.client'
+    REQUESTS_GET_PATH = BASE_PATH + 'requests.get'
+
+    @patch(PROCESS_FETCH_PATH, return_value=True)
+    @patch(EARLIEST_UNFETCHED_DATE_PATH, return_value='2023-01-01')
+    @patch(BOTO3_CLIENT_PATH)
+    @patch(REQUESTS_GET_PATH)
     @patch.dict(os.environ, {
         "AWS_DEFAULT_REGION": "us-east-1",
         "RESOURCE_ARN": "mock_resource_arn",
         "SECRET_ARN": "mock_secret_arn",
         "DATABASE_NAME": "mock_database"
     })
-    def test_successful_fetch_and_processing(self, mock_requests, mock_boto3, mock_get_earliest_unfetched_date, mock_process_fetch):
-        # Arrange
-        mock_requests.return_value = MagicMock(status_code=200, text='<xml>mock response</xml>')
+    def test_successful_fetch_and_processing(self,
+                                             mock_requests,
+                                             mock_boto3,
+                                             mock_get_earliest_unfetched_date,
+                                             mock_process_fetch):
+        mock_requests.return_value = MagicMock(status_code=200,
+                                               text='<xml>mock response</xml>')
         mock_boto3.return_value = MagicMock()
-        event = {'base_url': 'http://example.com', 'bucket_name': 'mybucket', 'summary_set': 'summary1'}
+        event = {
+            'base_url': 'http://example.com',
+            'bucket_name': 'mybucket',
+            'summary_set': 'summary1'}
         context = MagicMock()
 
         # Act
@@ -30,18 +48,23 @@ class TestLambdaHandler(unittest.TestCase):
         mock_requests.assert_called_once()
         mock_boto3.assert_called()
 
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.get_earliest_unfetched_date', return_value=None)
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.boto3.client')
+    @patch(EARLIEST_UNFETCHED_DATE_PATH, return_value=None)
+    @patch(BOTO3_CLIENT_PATH)
     @patch.dict(os.environ, {
         "AWS_DEFAULT_REGION": "us-east-1",
         "RESOURCE_ARN": "mock_resource_arn",
         "SECRET_ARN": "mock_secret_arn",
         "DATABASE_NAME": "mock_database"
     })
-    def test_no_data_to_fetch(self, mock_boto3, mock_get_earliest_unfetched_date):
+    def test_no_data_to_fetch(self,
+                              mock_boto3,
+                              mock_get_earliest_unfetched_date):
         # Arrange
         mock_boto3.return_value = MagicMock()
-        event = {'base_url': 'http://example.com', 'bucket_name': 'mybucket', 'summary_set': 'summary1'}
+        event = {
+            'base_url': 'http://example.com',
+            'bucket_name': 'mybucket',
+            'summary_set': 'summary1'}
         context = MagicMock()
 
         # Act
@@ -52,28 +75,37 @@ class TestLambdaHandler(unittest.TestCase):
         self.assertIn('No unfetched dates found', response['body'])
 
     @patch.dict(os.environ, {"AWS_DEFAULT_REGION": "us-east-1"})
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.get_earliest_unfetched_date', side_effect=Exception("Database error"))
-    @patch('services.fetch_daily_summaries.src.fetch_daily_summaries.boto3.client')
+    @patch(EARLIEST_UNFETCHED_DATE_PATH,
+           side_effect=Exception("Database error"))
+    @patch(BOTO3_CLIENT_PATH)
     @patch.dict(os.environ, {
         "AWS_DEFAULT_REGION": "us-east-1",
         "RESOURCE_ARN": "mock_resource_arn",
         "SECRET_ARN": "mock_secret_arn",
         "DATABASE_NAME": "mock_database"
     })
-    def test_error_handling_database_interaction(self, mock_boto3, mock_get_earliest_unfetched_date):
+    def test_error_handling_db_interaction(self,
+                                           mock_boto3,
+                                           mock_get_earliest_unfetched_date):
         # Mock the 'execute_statement' method of the RDS data client
         mock_client = MagicMock()
         mock_boto3.return_value = mock_client
         mock_client.execute_statement.return_value = {'some_key': 'some_value'}
 
         # Arrange
-        event = {'base_url': 'http://example.com', 'bucket_name': 'mybucket', 'summary_set': 'summary1'}
+        event = {
+            'base_url': 'http://example.com',
+            'bucket_name': 'mybucket',
+            'summary_set': 'summary1'}
         context = MagicMock()
 
         # Act
         response = lambda_handler(event, context)
         # Arrange
-        event = {'base_url': 'http://example.com', 'bucket_name': 'mybucket', 'summary_set': 'summary1'}
+        event = {
+            'base_url': 'http://example.com',
+            'bucket_name': 'mybucket',
+            'summary_set': 'summary1'}
         context = MagicMock()
 
         # Act
@@ -84,10 +116,16 @@ class TestLambdaHandler(unittest.TestCase):
         self.assertIn('Internal server error', response['body'])
 
     @patch.dict(os.environ, {"AWS_DEFAULT_REGION": "us-east-1"})
-    @patch.dict(os.environ, {'RESOURCE_ARN': '', 'SECRET_ARN': '', 'DATABASE_NAME': ''})
+    @patch.dict(os.environ, {
+        'RESOURCE_ARN': '',
+        'SECRET_ARN': '',
+        'DATABASE_NAME': ''})
     def test_missing_environment_variables(self):
         # Arrange
-        event = {'base_url': 'http://example.com', 'bucket_name': 'mybucket', 'summary_set': 'summary1'}
+        event = {
+            'base_url': 'http://example.com',
+            'bucket_name': 'mybucket',
+            'summary_set': 'summary1'}
         context = MagicMock()
 
         # Act
