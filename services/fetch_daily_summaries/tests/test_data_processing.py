404fb74ce8d6288650a4193af310df488343f1b0
Brad Edwards <j.bradley.edwards@gmail.com>
2023-12-02 23:26:59 -0800

    Add log_initial_info tests

--- services/fetch_daily_summaries/tests/test_data_processing.py
+++ services/fetch_daily_summaries/tests/test_data_processing.py
@@ -1,5 +1,7 @@
 # Test file for fetch_daily_summaries data processing functions
+import logging
 import unittest
+from unittest.mock import patch, MagicMock
 from services.fetch_daily_summaries.src.fetch_daily_summaries import (
     log_initial_info,
     get_event_params,
@@ -13,8 +15,46 @@ from services.fetch_daily_summaries.src.fetch_daily_summaries import (
 
 class TestLogInitialInfo(unittest.TestCase):
 
-    def test_log_initial_info(self):
-        self.assertEqual(True, False)
+    BASE_PATH = 'services.fetch_daily_summaries.src.fetch_daily_summaries.'
+    LOGGING_PATH = BASE_PATH + 'logging'
+    START_MESSAGE = "Starting to fetch arXiv daily summaries"
+
+    def setUp(self):
+        self.mock_logger = MagicMock()
+        self.patcher = patch(TestLogInitialInfo.LOGGING_PATH, self.mock_logger)
+        self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
+
+    def test_log_initial_info_with_valid_event(self):
+        # Test with a valid event dictionary
+        event = {'key': 'value'}
+        log_initial_info(event)
+        self.mock_logger.info.assert_any_call(f"Received event: {event}")
+        self.mock_logger.info.assert_any_call(TestLogInitialInfo.START_MESSAGE)
+
+    def test_log_initial_info_with_empty_event(self):
+        # Test with an empty event dictionary
+        event = {}
+        log_initial_info(event)
+        self.mock_logger.info.assert_any_call(f"Received event: {event}")
+        self.mock_logger.info.assert_any_call(TestLogInitialInfo.START_MESSAGE)
+
+    def test_log_initial_info_with_none_event(self):
+        # Test with a None event
+        event = None
+        log_initial_info(event)
+        self.mock_logger.info.assert_any_call(f"Received event: {event}")
+        self.mock_logger.info.assert_any_call(TestLogInitialInfo.START_MESSAGE)
+
+    def test_log_initial_info_with_unusual_event(self):
+        # Test with an unusual event type, like a string
+        event = "Some event"
+        log_initial_info(event)
+        self.mock_logger.info.assert_any_call(f"Received event: {event}")
+        self.mock_logger.info.assert_any_call(TestLogInitialInfo.START_MESSAGE)
+
 
 
 class TestGetEventParams(unittest.TestCase):
