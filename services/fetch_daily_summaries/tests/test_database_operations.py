a02c42b88c3e44fc96e539654d40d4124ba9cf9d
Brad Edwards <j.bradley.edwards@gmail.com>
2023-12-17 23:11:24 -0800

    93 bug date str type mismatch (#98)
    * Fix method and test for fetch status now is a date not a string
    
    * Lint
    
    * Lint
    
    * Cast param to date
    
    * Fix SQL string formatting
    
    * Refactor for moving event params to env vars
    
    * Refactor to use date instead of string

--- services/fetch_daily_summaries/tests/test_database_operations.py
+++ services/fetch_daily_summaries/tests/test_database_operations.py
@@ -2,7 +2,7 @@
 import logging
 import unittest
 from unittest.mock import patch, Mock
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from services.fetch_daily_summaries.src.fetch_daily_summaries import (
     insert_fetch_status,
     get_earliest_unfetched_date,
@@ -26,7 +26,7 @@ class TestInsertFetchStatus(unittest.TestCase):
         mock_boto3_client.return_value = mock_client
         mock_client.execute_statement.return_value = {"ResponseMetadata": {"HTTPStatusCode": 200}}
 
-        response = insert_fetch_status("2023-01-01", "mock_aurora_arn", "mock_secret_arn", "mock_database")
+        response = insert_fetch_status(date(2023, 1, 1), "mock_aurora_arn", "mock_secret_arn", "mock_database")
 
         mock_boto3_client.assert_called_once_with("rds-data")
         mock_client.execute_statement.assert_called_once()
@@ -39,7 +39,7 @@ class TestInsertFetchStatus(unittest.TestCase):
         mock_client.execute_statement.side_effect = Exception(DATABASE_ERROR)
 
         with self.assertRaises(Exception) as context:
-            insert_fetch_status("2023-01-01", "mock_aurora_arn", "mock_secret_arn", "mock_database")
+            insert_fetch_status(date(2023, 1, 1), "mock_aurora_arn", "mock_secret_arn", "mock_database")
 
         self.assertEqual(str(context.exception), DATABASE_ERROR)
 
@@ -100,13 +100,13 @@ class TestSetFetchStatus(unittest.TestCase):
         mock_boto3_client.return_value = mock_client
         mock_client.execute_statement.return_value = {"update_result": "success"}
 
-        date = "2023-01-01"
+        test_date = date(2023, 1, 1)
         status = "success"
         aurora_cluster_arn = "mock_aurora_cluster_arn"
         db_credentials_secret_arn = "mock_db_credentials_secret_arn"
         database = "mock_database"
 
-        result = set_fetch_status(date, status, aurora_cluster_arn, db_credentials_secret_arn, database)
+        result = set_fetch_status(test_date, status, aurora_cluster_arn, db_credentials_secret_arn, database)
         self.assertTrue(result)
 
     @patch(BOTO3_CLIENT_PATH)
@@ -115,11 +115,11 @@ class TestSetFetchStatus(unittest.TestCase):
         mock_boto3_client.return_value = mock_client
         mock_client.execute_statement.side_effect = Exception(DATABASE_ERROR)
 
-        date = "2023-01-01"
+        test_date = date(2023, 1, 1)
         status = "failure"
         aurora_cluster_arn = "mock_aurora_cluster_arn"
         db_credentials_secret_arn = "mock_db_credentials_secret_arn"
         database = "mock_database"
 
-        result = set_fetch_status(date, status, aurora_cluster_arn, db_credentials_secret_arn, database)
+        result = set_fetch_status(test_date, status, aurora_cluster_arn, db_credentials_secret_arn, database)
         self.assertFalse(result)
