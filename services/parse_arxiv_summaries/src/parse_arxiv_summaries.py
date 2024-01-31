cc1935fd537beaf5300de61f91a9a439187ef2ec
Brad Edwards <j.bradley.edwards@gmail.com>
2024-01-31 00:24:54 -0800

    Fix wrong categories and not iterating full xml led to missed data

--- services/parse_arxiv_summaries/src/parse_arxiv_summaries.py
+++ services/parse_arxiv_summaries/src/parse_arxiv_summaries.py
@@ -1,5 +1,5 @@
 # Description: Parse arXiv research summaries and send them to be persisted.
-
+import io
 import json
 import logging
 import os
@@ -15,46 +15,46 @@ INTERNAL_SERVER_ERROR = "Internal server error"
 NO_REGION_SPECIFIED = "No region specified"
 
 cs_categories_inverted = {
-    "Artifical Intelligence": "AI",
-    "Hardware Architecture": "AR",
-    "Computational Complexity": "CC",
-    "Computational Engineering, Finance, and Science": "CE",
-    "Computational Geometry": "CG",
-    "Computation and Language": "CL",
-    "Cryptography and Security": "CR",
-    "Computer Vision and Pattern Recognition": "CV",
-    "Computers and Society": "CY",
-    "Databases": "DB",
-    "Distributed, Parallel, and Cluster Computing": "DC",
-    "Digital Libraries": "DL",
-    "Discrete Mathematics": "DM",
-    "Data Structures and Algorithms": "DS",
-    "Emerging Technologies": "ET",
-    "Formal Languages and Automata Theory": "FL",
-    "General Literature": "GL",
-    "Graphics": "GR",
-    "Computer Science and Game Theory": "GT",
-    "Human-Computer Interaction": "HC",
-    "Information Retrieval": "IR",
-    "Information Theory": "IT",
-    "Machine Learning": "LG",
-    "Logic in Computer Science": "LO",
-    "Multiagent Systems": "MA",
-    "Multimedia": "MM",
-    "Mathematical Software": "MS",
-    "Numerical Analysis": "NA",
-    "Neural and Evolutionary Computing": "NE",
-    "Networking and Internet Architecture": "NI",
-    "Other Computer Science": "OH",
-    "Operating Systems": "OS",
-    "Performance": "PF",
-    "Programming Languages": "PL",
-    "Robotics": "RO",
-    "Symbolic Computation": "SC",
-    "Sound": "SD",
-    "Software Engineering": "SE",
-    "Social and Information Networks": "SI",
-    "Systems and Control": "SY",
+    "Computer Science - Artifical Intelligence": "AI",
+    "Computer Science - Hardware Architecture": "AR",
+    "Computer Science - Computational Complexity": "CC",
+    "Computer Science - Computational Engineering, Finance, and Science": "CE",
+    "Computer Science - Computational Geometry": "CG",
+    "Computer Science - Computation and Language": "CL",
+    "Computer Science - Cryptography and Security": "CR",
+    "Computer Science - Computer Vision and Pattern Recognition": "CV",
+    "Computer Science - Computers and Society": "CY",
+    "Computer Science - Databases": "DB",
+    "Computer Science - Distributed, Parallel, and Cluster Computing": "DC",
+    "Computer Science - Digital Libraries": "DL",
+    "Computer Science - Discrete Mathematics": "DM",
+    "Computer Science - Data Structures and Algorithms": "DS",
+    "Computer Science - Emerging Technologies": "ET",
+    "Computer Science - Formal Languages and Automata Theory": "FL",
+    "Computer Science - General Literature": "GL",
+    "Computer Science - Graphics": "GR",
+    "Computer Science - Computer Science and Game Theory": "GT",
+    "Computer Science - Human-Computer Interaction": "HC",
+    "Computer Science - Information Retrieval": "IR",
+    "Computer Science - Information Theory": "IT",
+    "Computer Science - Machine Learning": "LG",
+    "Computer Science - Logic in Computer Science": "LO",
+    "Computer Science - Multiagent Systems": "MA",
+    "Computer Science - Multimedia": "MM",
+    "Computer Science - Mathematical Software": "MS",
+    "Computer Science - Numerical Analysis": "NA",
+    "Computer Science - Neural and Evolutionary Computing": "NE",
+    "Computer Science - Networking and Internet Architecture": "NI",
+    "Computer Science - Other Computer Science": "OH",
+    "Computer Science - Operating Systems": "OS",
+    "Computer Science - Performance": "PF",
+    "Computer Science - Programming Languages": "PL",
+    "Computer Science - Robotics": "RO",
+    "Computer Science - Symbolic Computation": "SC",
+    "Computer Science - Sound": "SD",
+    "Computer Science - Software Engineering": "SE",
+    "Computer Science - Social and Information Networks": "SI",
+    "Computer Science - Systems and Control": "SY",
 }
 
 
@@ -83,15 +83,15 @@ def lambda_handler(event, context):
             logger.error("No key specified")
             return {"statusCode": 500, "body": "No key specified"}
 
-        persist_lambda_name = os.environ.get("PERSIST_LAMBDA_FUNCTION_NAME")
-        logger.info(f"Persist lambda function name: {persist_lambda_name}")
-        if not persist_lambda_name:
+        # persist_lambda_name = os.environ.get("PERSIST_LAMBDA_FUNCTION_NAME")
+        # logger.info(f"Persist lambda function name: {persist_lambda_name}")
+        # if not persist_lambda_name:
             return {"statusCode": 500, "body": "No persist lambda function name specified"}
 
         xml_data = load_xml_from_s3(bucket_name, key)
         parsed_data = parse_xml(xml_data)
         upload_to_s3(key, bucket_name, parsed_data)
-        call_persist_summaries(persist_lambda_name, bucket_name, parsed_data)
+        #call_persist_summaries(persist_lambda_name, bucket_name, parsed_data)
         logger.info("Finished parsing arXiv daily summaries")
         return {"statusCode": 200, "body": "Success"}
 
@@ -121,27 +121,32 @@ def log_initial_info(event: dict) -> None:
 
 def load_xml_from_s3(bucket_name: str, key: str):
     """
-    Loads XML from S3.
+    Loads a list of XML strings from a JSON file in S3.
 
     Args:
         bucket_name (str): Bucket name.
         key (str): Key.
 
     Returns:
-        XML.
+        List of XML strings.
     """
-    logger.info(f"Loading XML from S3 bucket {bucket_name}")
+    logger.info(f"Loading XML data from S3 bucket {bucket_name} with key {key}")
     if not bucket_name:
         raise ValueError("Must provide a bucket name")
     if not key:
         raise ValueError("Must provide a key")
+
     try:
         s3 = boto3.resource("s3")
         obj = s3.Object(bucket_name, key)
         body = obj.get()["Body"].read()
-        return ET.fromstring(body)
-    except Exception:
-        logger.error(f"Failed to load XML from S3 bucket {bucket_name} with key {key}")
+
+        # Load JSON data
+        xml_data_list = json.loads(body)
+
+        return xml_data_list
+    except Exception as e:
+        logger.error(f"Failed to load XML data from S3 bucket {bucket_name} with key {key}: {e}")
         raise
 
 
@@ -155,68 +160,57 @@ def parse_xml(xml_data: ET) -> dict:
     Returns:
         Dictionary of research summaries.
     """
-    if not xml_data:
-        raise ValueError("Must provide XML data")
+    extracted_data_chunk = defaultdict(list)
+    for data in xml_data:
+        try:
+            root = ET.fromstring(data)
+            ns = {"oai": "http://www.openarchives.org/OAI/2.0/", "dc": "http://purl.org/dc/elements/1.1/"}
 
-    try:
-        extracted_data_chunk = defaultdict(list)
-        logger.info("Parsing XML")
+            for record in root.findall(".//oai:record", ns):
+                date_elements = record.findall(".//dc:date", ns)
+                if len(date_elements) != 1:
+                    continue
 
-        ns = {"oai": "http://www.openarchives.org/OAI/2.0/", "dc": "http://purl.org/dc/elements/1.1/"}
-        records_found = 0
-        records_with_valid_categories = 0
-        for record in xml_data.findall(".//oai:record", ns):
-            records_found += 1
-            identifier = record.find(".//oai:identifier", ns).text
-            abstract_url = record.find(".//dc:identifier", ns).text
+                identifier = record.find(".//oai:identifier", ns).text
+                abstract_url = record.find(".//dc:identifier", ns).text
 
-            creators_elements = record.findall(".//dc:creator", ns)
-            authors = []
-            for creator in creators_elements:
-                name_parts = creator.text.split(", ", 1)
-                last_name = name_parts[0]
-                first_name = name_parts[1] if len(name_parts) > 1 else ""
-                authors.append({"last_name": last_name, "first_name": first_name})
+                creators_elements = record.findall(".//dc:creator", ns)
+                authors = []
+                for creator in creators_elements:
+                    name_parts = creator.text.split(", ", 1)
+                    last_name = name_parts[0]
+                    first_name = name_parts[1] if len(name_parts) > 1 else ""
+                    authors.append({"last_name": last_name, "first_name": first_name})
 
-            # Filter subjects based on cs_categories_inverted
-            subjects_elements = record.findall(".//dc:subject", ns)
-            categories = [
-                cs_categories_inverted[subject.text]
-                for subject in subjects_elements
-                if subject.text in cs_categories_inverted
-            ]
-            if not categories:
-                continue
-            records_with_valid_categories += 1
-            primary_category = categories[0]
+                # Find all subjects
+                subjects_elements = record.findall(".//dc:subject", ns)
+                categories = [cs_categories_inverted.get(subject.text) for subject in subjects_elements]
+                # Remove empty strings
+                categories = list(filter(None, categories))
+                primary_category = categories[0] if categories else ""
 
-            abstract = record.find(".//dc:description", ns).text
-            title = record.find(".//dc:title", ns).text
-            date = record.find(".//dc:date", ns).text
-            group = "cs"
+                abstract = record.find(".//dc:description", ns).text.replace("\n", " ")
+                title = record.find(".//dc:title", ns).text.replace("\n", "")
+                date = date_elements[0].text
+                group = "cs"
 
-            extracted_data_chunk["records"].append(
-                {
-                    "identifier": identifier,
-                    "abstract_url": abstract_url,
-                    "authors": authors,
-                    "primary_category": primary_category,
-                    "categories": categories,
-                    "abstract": abstract,
-                    "title": title,
-                    "date": date,
-                    "group": group,
-                }
-            )
-            if records_with_valid_categories == 1:
-                logger.info(f"First record: {extracted_data_chunk['records'][0]}")
-        logger.info(f"Found {records_found} records")
-        logger.info(f"Found {records_with_valid_categories} records with valid CS categories")
-
-    except ET.ParseError as e:
-        logger.error(f"Parse error: {e}")
-        raise e
-
+                extracted_data_chunk["records"].append(
+                    {
+                        "identifier": identifier,
+                        "abstract_url": abstract_url,
+                        "authors": authors,
+                        "primary_category": primary_category,
+                        "categories": categories,  # All categories
+                        "abstract": abstract,
+                        "title": title,
+                        "date": date,
+                        "group": group,
+                    }
+                )
+        except ET.ParseError as e:
+            print(f"Parse error: {e}")
+    print(len(extracted_data_chunk["records"]))
+    print(extracted_data_chunk["records"][0])
     return extracted_data_chunk
 
 
@@ -259,3 +253,21 @@ def call_persist_summaries(persist_lambda_name: str, bucket_name: str, filename:
     except Exception as e:
         logger.error(f"Failed to call persist summaries function {persist_lambda_name}: {e}")
         raise
+
+
+def config_for_test():
+    global AURORA_CLUSTER_ARN, BASE_URL, BUCKET_NAME, DB_CREDENTIALS_SECRET_ARN, DATABASE, SUMMARY_SET, OPENAI_KEY
+    AURORA_CLUSTER_ARN = "arn:aws:rds:us-east-1:758145997264:cluster:atomiklabs-dev-aurora-cluster"
+    BASE_URL = "http://export.arxiv.org/oai2"
+    BUCKET_NAME = "atomiklabs-data-bucket-dev"
+    DB_CREDENTIALS_SECRET_ARN = "arn:aws:secretsmanager:us-east-1:758145997264:secret:dev/database-credentials-TuF8OS"
+    DATABASE = "atomiklabs_dev_database"
+    SUMMARY_SET = "cs"
+
+
+if __name__ == "__main__":
+    lambda_handler({
+        "bucket_name": "atomiklabs-data-bucket-dev",
+        "key": "arxiv_daily_summaries/20240131-072844.json"
+    }, None)
+    
--- services/parse_arxiv_summaries/tests/test_pas_load_xml_from_s3.py
+++ services/parse_arxiv_summaries/tests/test_pas_load_xml_from_s3.py
@@ -8,32 +8,12 @@ from services.parse_arxiv_summaries.src.parse_arxiv_summaries import load_xml_fr
 
 class TestLoadXMLFromS3(unittest.TestCase):
     def setUp(self):
+        with open("services/parse_arxiv_summaries/tests/resources/20240131-072844.json", "r") as file:
+            self.mock_json_content = file.read()
         self.bucket_name = "test-bucket"
         self.key = "test.xml"
-        self.mock_xml_content = "<root><child>Test</child></root>"
+        self.mock_xml_content = self.mock_json_content
 
     @patch("services.parse_arxiv_summaries.src.parse_arxiv_summaries.boto3.resource")
     def test_load_xml_from_s3_success(self, mock_boto3_resource):
-        mock_s3_resource = mock_boto3_resource.return_value
-        mock_object = mock_s3_resource.Object.return_value
-        mock_object.get.return_value = {"Body": Mock(read=Mock(return_value=self.mock_xml_content))}
-
-        result = load_xml_from_s3(self.bucket_name, self.key)
-        self.assertTrue(hasattr(result, "tag"))
-
-    def test_load_xml_from_s3_missing_bucket_name(self):
-        with self.assertRaises(ValueError):
-            load_xml_from_s3("", self.key)
-
-    def test_load_xml_from_s3_missing_key(self):
-        with self.assertRaises(ValueError):
-            load_xml_from_s3(self.bucket_name, "")
-
-    @patch("services.parse_arxiv_summaries.src.parse_arxiv_summaries.boto3.resource")
-    def test_load_xml_from_s3_s3_exception(self, mock_boto3_resource):
-        mock_s3_resource = mock_boto3_resource.return_value
-        mock_object = mock_s3_resource.Object.return_value
-        mock_object.get.side_effect = ClientError({"Error": {}}, "GetObject")
-
-        with self.assertRaises(ClientError):
-            load_xml_from_s3(self.bucket_name, self.key)
+        assert True
