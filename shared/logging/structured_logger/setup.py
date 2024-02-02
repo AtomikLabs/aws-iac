2b55aba63af1566be061f6880edc492b93632824
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-02 09:57:25 -0800

    Add structured logger shared package skeleton, update requirements files

---
+++ shared/logging/structured_logger/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='structured_logger',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A structured logger for Python',
+    install_requires=[
+    ],
+)
