cce47b472e1c6e32755fbb2d1e8571027ba7a8c4
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-12 15:50:33 -0800

    Add ECR and policy

--- infra/containerization/ecr-iam.tf
+++ infra/containerization/ecr-iam.tf
@@ -2,16 +2,22 @@ resource "aws_ecr_repository_policy" "repo_access_policy" {
   repository = aws_ecr_repository.repo.name
 
   policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        Effect   = "Allow"
-        Action   = [
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
           "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:GetAuthorizationToken",
-        ]
-        Resource = "*"
+          "ecr:BatchGetImage"
+        ],
+        "Resource": "${aws_ecr_repository.repo.arn}"
+      },
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "ecr:GetAuthorizationToken",
+        "Resource": "*"
       }
     ]
   })
