e0c96fb744000fc11ea4b357c1effa8c034869a8
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-11 19:23:37 -0800

    Fix outputs variable naming

--- infra/network/outputs.tf
+++ infra/network/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.app_vpc.id
+  value = aws_vpc.atomiklabs_vpc.id
+  description = "The ID of the VPC"
 }
 
-output "subnet_ids" {
-  value = aws_subnet.app_subnet[*].id
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public_subnet : s.id]
+  description = "The IDs of the public subnets"
 }
 
 output "security_group_id" {
-  value = aws_security_group.app_sg.id
+  value = aws_security_group.public_sg.id
+  description = "The ID of the security group for public subnets"
 }
