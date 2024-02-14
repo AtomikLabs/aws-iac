4920d99405dd1d7e46cebe4e0dc6d7b4fd2da14c
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-13 21:24:18 -0800

    Re-factor EKS to support Kafka - leverage DoEKS

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,23 +1,41 @@
-variable "REGION" {
+variable "region" {
+  description = "REGION"
   type        = string
-  description = "AWS region"
-}
-variable "ENVIRONMENT_NAME" {
-  type        = string
-  description = "Name of the deployment environment"
+  default     = "us-east-1"
 }
 
-variable "VPC_CIDR" {
+variable "name" {
+  description = "Name of the VPC and EKS Cluster"
   type        = string
-  description = "CIDR block for the VPC"
+  default     = "atomiklabs"
 }
 
-variable "SUBNET_CIDRS" {
-  type        = map(list(string))
-  description = "Map of subnet CIDRs"
+variable "eks_cluster_version" {
+  description = "EKS Cluster version"
+  type        = string
+  default     = "1.29"
 }
 
-variable "AVAILABILITY_ZONES" {
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "enable_amazon_prometheus" {
+  description = "Enable AWS Managed Prometheus service"
+  type        = bool
+  default     = true
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "availability_zones" {
+  description = "Availability Zones"
   type        = list(string)
-  description = "List of availability zones in which to create subnets"
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
