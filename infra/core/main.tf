b322be6d2ffee5a6dfe7ffbf8f54b1ff6e523b0e
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-17 08:32:01 -0800

    Fix spacing

--- infra/core/main.tf
+++ infra/core/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "atomiklabs-infra-config-bucket"
     key            = "terraform/terraform.state"
-    region     = "us-east-1"
+    region         = "us-east-1"
     dynamodb_table = "atomiklabs-terraform-locks"
     encrypt        = true
   }
