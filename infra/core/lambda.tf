b7b77250d297b33c9ee4f705826468b21e93c01d
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-19 11:04:56 -0800

    Add source ARN

--- infra/core/lambda.tf
+++ infra/core/lambda.tf
@@ -101,4 +101,5 @@ resource "aws_lambda_permission" "allow_eventbridge_to_invoke_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.fetch_daily_summaries.function_name
   principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.fetch_daily_summaries.arn
 }
