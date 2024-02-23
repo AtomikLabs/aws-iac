f371074c51cb19506e13688433c91824a05f4f79
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-22 17:34:36 -0800

    Downgrade rabbitmq instances

--- infra/core/messaging.tf
+++ infra/core/messaging.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "rabbitmq" {
   count                     = 2
   ami                       = "ami-0440d3b780d96b29d"
-  instance_type             = "t3.micro"
+  instance_type             = "t2.micro"
   subnet_id                 = aws_subnet.private[count.index].id
   key_name                  = "${local.environment}-${local.bastion_host_key_pair_name}"
   vpc_security_group_ids    = [aws_security_group.rabbitmq_sg.id]
