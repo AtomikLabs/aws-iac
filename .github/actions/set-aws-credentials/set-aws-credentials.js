0c52390f393401de380c827f074e124a85752a7e
Brad Edwards <j.bradley.edwards@gmail.com>
2024-02-24 11:11:07 -0800

    Fix secrets not passed to action

--- .github/actions/set-aws-credentials/set-aws-credentials.js
+++ .github/actions/set-aws-credentials/set-aws-credentials.js
@@ -7,23 +7,28 @@ permittedEnvironments = ['prod', 'stage', 'dev'];
 function setAwsCredentials() {
   console.log('Setting AWS credentials...');
   try {
-    const environmentName = core.getInput('environment_name');
-    const awsRegion = core.getInput('aws_region');
+    const environmentName = core.getInput('AWS_REGION');
+    const DEV_AWS_ACCESS_KEY_ID = core.getInput('DEV_AWS_ACCESS_KEY_ID');
+    const DEV_AWS_SECRET_ACCESS_KEY = core.getInput('DEV_AWS_SECRET_ACCESS_KEY');
+    const PROD_AWS_ACCESS_KEY_ID = core.getInput('PROD_AWS_ACCESS_KEY_ID');
+    const PROD_AWS_SECRET_ACCESS_KEY = core.getInput('PROD_AWS_SECRET_ACCESS_KEY');
+    const STAGE_AWS_ACCESS_KEY_ID = core.getInput('STAGE_AWS_ACCESS_KEY_ID');
+    const STAGE_AWS_SECRET_ACCESS_KEY = core.getInput('STAGE_AWS_SECRET_ACCESS_KEY');
     let awsAccessKeyId = '';
     let awsSecretAccessKey = '';
 
     switch(environmentName) {
+      case 'dev':
+        awsAccessKeyId = DEV_AWS_ACCESS_KEY_ID;
+        awsSecretAccessKey = DEV_AWS_SECRET_ACCESS_KEY;
+        break;
       case 'prod':
-        awsAccessKeyId = process.env.PROD_AWS_ACCESS_KEY_ID;
-        awsSecretAccessKey = process.env.PROD_AWS_SECRET_ACCESS_KEY;
+        awsAccessKeyId = PROD_AWS_ACCESS_KEY_ID;
+        awsSecretAccessKey = PROD_AWS_SECRET_ACCESS_KEY;
         break;
       case 'stage':
-        awsAccessKeyId = process.env.STAGE_AWS_ACCESS_KEY_ID;
-        awsSecretAccessKey = process.env.STAGE_AWS_SECRET_ACCESS_KEY;
-        break;
-      case 'dev':
-        awsAccessKeyId = process.env.DEV_AWS_ACCESS_KEY_ID;
-        awsSecretAccessKey = process.env.DEV_AWS_SECRET_ACCESS_KEY;
+        awsAccessKeyId = STAGE_AWS_ACCESS_KEY_ID;
+        awsSecretAccessKey = STAGE_AWS_SECRET_ACCESS_KEY;
         break;
       default:
         throw new Error(`Unsupported environment name: ${environmentName}`);
@@ -40,5 +45,4 @@ function setAwsCredentials() {
   }
 }
 
-module.exports = { setAwsCredentials };
 setAwsCredentials();
