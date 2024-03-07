7da21e699a85eadecfeedf27f012a9098084da43
Brad Edwards <j.bradley.edwards@gmail.com>
2024-03-06 18:44:13 -0800

    Fix var naming in outputs

--- infra/core/outputs.tf
+++ infra/core/outputs.tf
@@ -10,42 +10,42 @@ output "repo_url" {
 # **********************************************************
 output "aws_glue_catalog_database_data_catalog_database_arn" {
   description = "AWS Glue Catalog Database ARN for data catalog"
-  value       = module.data_management.aws_glue_catalog_database.data_catalog_database.arn
+  value       = module.data_management.aws_glue_catalog_database_data_catalog_database_arn
 }
 
 output "aws_glue_catalog_database_data_catalog_database_id" {
   description = "AWS Glue Catalog Database ID for data catalog"
-  value       = module.data_management.aws_glue_catalog_database.data_catalog_database.id
+  value       = module.data_management.aws_glue_catalog_database_data_catalog_database_id
 }
 
 output "aws_glue_catalog_database_data_catalog_database_name" {
   description = "AWS Glue Catalog Database for data catalog"
-  value       = module.data_management.aws_glue_catalog_database.data_catalog_database.name
+  value       = module.data_management.aws_glue_catalog_database_data_catalog_database_name
 }
 
 output "aws_glue_catalog_table_data_catalog_table_data_ingestion_metadata_table_arn" {
     description = "AWS Glue Catalog Table ARN for data ingestion metadata"
-    value       = module.data_management.aws_glue_catalog_table.data_ingestion_metadata_table.arn
+    value       = module.data_management.aws_glue_catalog_table_data_catalog_table_data_ingestion_metadata_table_arn
 }
 
 output "aws_glue_catalog_table_data_catalog_table_data_ingestion_metadata_table_id" {
   description = "AWS Glue Catalog Table ID for data ingestion metadata"
-  value       = module.data_management.aws_glue_catalog_table.data_ingestion_metadata_table.id
+  value       = module.data_management.aws_glue_catalog_table_data_catalog_table_data_ingestion_metadata_table_id
 }
 
 output "aws_glue_catalog_table_data_catalog_table_data_ingestion_metadata_table_name" {
   description = "AWS Glue Catalog Table for data ingestion metadata"
-  value       = module.data_management.aws_glue_catalog_table.data_ingestion_metadata_table.name
+  value       = module.data_management.aws_glue_catalog_table_data_catalog_table_data_ingestion_metadata_table_name
 }
 
 output "aws_iam_policy_s3_infra_config_bucket_access" {
     description = "IAM policy for S3 infra config bucket access"
-    value       = module.data_management.aws_iam_policy.s3_infra_config_bucket_access.arn
+    value       = module.data_management.aws_iam_policy_s3_infra_config_bucket_access
 }
 
 output "aws_s3_bucket_atomiklabs_data_bucket" {
   description = "S3 bucket for atomiklabs data"
-  value       = module.data_management.aws_s3_bucket.atomiklabs_data_bucket.bucket
+  value       = module.data_management.aws_s3_bucket_atomiklabs_data_bucket
 }
 
 # **********************************************************
