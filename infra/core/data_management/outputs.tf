674eff061acb4b4a5b73317fd123889d7ed21bbc
Brad Edwards <j.bradley.edwards@gmail.com>
2024-03-07 22:06:37 -0800

    Add missing output

--- infra/core/data_management/outputs.tf
+++ infra/core/data_management/outputs.tf
@@ -37,3 +37,8 @@ output "aws_s3_bucket_atomiklabs_data_bucket" {
   description = "S3 bucket for atomiklabs data"
   value       = aws_s3_bucket.atomiklabs_data_bucket.bucket
 }
+
+output "aws_s3_bucket_atomiklabs_data_bucket_arn" {
+  description = "S3 bucket ARN for atomiklabs data"
+  value       = aws_s3_bucket.atomiklabs_data_bucket.arn
+}
