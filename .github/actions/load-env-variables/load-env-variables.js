24928797b92a2974a519c0004663bf58d8c00df7
Brad Edwards <j.bradley.edwards@gmail.com>
2024-03-16 23:45:56 -0700

    Add back env detection

---
+++ .github/actions/load-env-variables/load-env-variables.js
@@ -0,0 +1,40 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+
+async function loadEnvironmentVariables() {
+  try {
+    const eventName = github.context.eventName;
+    let refName;
+
+    if (eventName === "pull_request") {
+      refName = github.context.payload.pull_request.base.ref;
+    } else if (eventName === "push") {
+      const refParts = github.context.ref.split("/");
+      refName = refParts.length > 2 ? refParts[2] : "";
+    } else {
+      throw new Error(
+        "Unsupported GitHub event. Only pull_request and push events are supported."
+      );
+    }
+
+    if (!["dev", "stage", "prod"].includes(refName)) {
+      throw new Error(
+        `The branch ${refName} does not map to a supported environment.`
+      );
+    }
+
+    const envName = refName.replace(/\//g, "-");
+    core.exportVariable("ENV_NAME", envName);
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error}`);
+  }
+}
+
+if (require.main === module) {
+  loadEnvironmentVariables();
+}
+
+module.exports = {
+  loadEnvironmentVariables,
+};
