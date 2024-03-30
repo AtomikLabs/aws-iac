4c4775cefa34454b6ff7b1c41e08843c98398603
Brad Edwards <j.bradley.edwards@gmail.com>
2024-03-30 15:33:14 -0700

    Refactor models to properly use ABC
    Add arXiv set model
    
    Add tests

--- services/services_layer/src/services_layer/models.py
+++ services/services_layer/src/services_layer/models.py
@@ -1,5 +1,10 @@
+import uuid
+from abc import ABC, abstractmethod
+
 import structlog
-from neo4j import GraphDatabase
+from constants import FAILED_TO_CREATE_ARXIV_SET
+from neo4j import Driver
+from utils import validate_strings
 
 structlog.configure(
     [
@@ -13,15 +18,135 @@ structlog.configure(
 )
 
 
-class BaseModel:
-    def __init__(self, driver: GraphDatabase.driver):
-        self.driver = driver
+class BaseModel(ABC):
+    def __init__(self, driver: Driver, db: str = "neo4j"):
         self.logger = structlog.get_logger()
+        if not driver or not isinstance(driver, Driver):
+            self.logger.error("Invalid driver", method=self.__init__.__name__, driver=driver, db=db)
+            raise ValueError("Invalid driver")
+        if not validate_strings(db):
+            self.logger.error("Invalid database name", method=self.__init__.__name__, driver=driver, db=db)
+            raise ValueError("Invalid database name")
+        self.driver = driver
+        self.db = db
+        self.relationships = []
+        self.uuid = None
+
+    @abstractmethod
+    def create(self) -> dict:
+        pass
+
+    @abstractmethod
+    def load(self) -> dict:
+        pass
 
     def verify_connection(self):
         try:
             self.driver.verify_connectivity()
             self.logger.debug("Connection verified", method=self.verify_connection.__name__)
+        except AttributeError as e:
+            self.logger.error(
+                "Driver is of the wrong type, was a new value assigned to the driver?",
+                method=self.verify_connection.__name__,
+                error=str(e),
+            )
+            raise e
         except Exception as e:
             self.logger.error("Connection failed", method=self.verify_connection.__name__, error=str(e))
+            raise e
+
+
+class ArxivSet(BaseModel):
+    def __init__(self, driver: Driver, code: str, name: str):
+        super().__init__(driver)
+        if not validate_strings(code):
+            self.logger.error("Invalid code", method=self.__init__.__name__, code=code)
+            raise ValueError("Invalid code")
+        self.code = code
+        self.name = name
+        self.uuid = None
+        self.created = None
+        self.last_modified = None
+
+    def create(self):
+        if not validate_strings(self.code, self.name):
+            self.logger.error("Invalid code or name", method=self.create.__name__, code=self.code, name=self.name)
+            raise ValueError("Invalid code or name")
+        try:
+            self.verify_connection()
+            self.logger.debug("Creating ArxivSet", method=self.create.__name__, code=self.code, name=self.name)
+            records, summary, _ = self.driver.execute_query(
+                """
+                MERGE (a:ArxivSet {code: $code})
+                ON CREATE SET a.created=TIMESTAMP(), a.last_modified=TIMESTAMP(), a.uuid=$uuid, a.name=$name
+                RETURN a""",
+                name=self.name,
+                code=self.code,
+                uuid=str(uuid.uuid4()),
+                database_=self.db,
+            )
+            if records and summary.counters.nodes_created == 1:
+                self.logger.debug("ArxivSet created", method=self.create.__name__, code=self.code, name=self.name)
+            elif records and summary.counters.nodes_created == 0:
+                self.logger.debug(
+                    "ArxivSet already exists", method=self.create.__name__, code=self.code, name=self.name
+                )
+            else:
+                self.logger.error(
+                    FAILED_TO_CREATE_ARXIV_SET, method=self.create.__name__, code=self.code, name=self.name
+                )
+                raise RuntimeError()
+            data = records[0].data()
+            self.name = data["a"]["name"]
+            self.uuid = data["a"]["uuid"]
+            self.created = data["a"]["created"]
+            self.last_modified = data["a"]["last_modified"]
+            if not validate_strings(self.uuid, self.created, self.last_modified):
+                self.logger.error(
+                    "Failed to properly create ArxivSet",
+                    method=self.create.__name__,
+                    code=self.code,
+                    name=self.name,
+                    uuid=self.uuid,
+                    created=self.created,
+                    last_modified=self.last_modified,
+                )
+                raise ValueError("Failed to create ArxivSet")
+        except Exception as e:
+            self.logger.error("Error while creating ArxivSet", method=self.create.__name__, error=str(e))
+            raise e
+
+    def load(self) -> bool:
+        if not validate_strings(self.code):
+            self.logger.error("Invalid code", method=self.load.__name__, code=self.code, name=self.name)
+            raise ValueError("Invalid code")
+        try:
+            self.verify_connection()
+            self.logger.debug("Loading ArxivSet", method=self.load.__name__, code=self.code, name=self.name)
+            records, _, _ = self.driver.execute_query(
+                "MATCH (a:ArxivSet {code: $code}) RETURN a", code=self.code, database_=self.db
+            )
+            if records:
+                self.logger.debug("ArxivSet loaded", method=self.load.__name__, code=self.code, name=self.name)
+                self.code = records[0]["a"]["code"]
+                self.name = records[0]["a"]["name"]
+                self.uuid = records[0]["a"]["uuid"]
+                self.created = records[0]["a"]["created"]
+                self.last_modified = records[0]["a"]["last_modified"]
+                if not validate_strings(self.code, self.name, self.uuid, self.created, self.last_modified):
+                    self.logger.error(
+                        "Failed to properly load ArxivSet",
+                        method=self.load.__name__,
+                        code=self.code,
+                        name=self.name,
+                        uuid=self.uuid,
+                        created=self.created,
+                        last_modified=self.last_modified,
+                    )
+                    raise ValueError("Failed to load ArxivSet")
+            return True if records else False
+        except Exception as e:
+            self.logger.error(
+                "Failed to load ArxivSet", method=self.load.__name__, error=str(e), code=self.code, name=self.name
+            )
             raise e
--- services/services_layer/tests/test_models.py
+++ services/services_layer/tests/test_models.py
@@ -1,40 +1,48 @@
 from unittest.mock import MagicMock, patch
 
 import pytest
-from neo4j import GraphDatabase
+from neo4j import Driver
 
-from services.services_layer.src.services_layer.models import BaseModel
+from services.services_layer.src.services_layer.models import BaseModel, ArxivSet
+
+
+class BaseModelChild(BaseModel):
+    def create(self):
+        pass # required to test instantiate abstract class
+
+    def load(self):
+        pass # required to test instantiate abstract class
 
 
 class TestBaseModel:
     @pytest.fixture(autouse=True)
     def setup(self):
-        self.driver_mock = MagicMock(spec=GraphDatabase.driver)
+        self.driver_mock = MagicMock(spec=Driver)
         self.driver_mock.verify_connectivity = MagicMock()
-        self.base_model = BaseModel(self.driver_mock)
+        self.base_model = BaseModelChild(self.driver_mock)
 
     def test_init(self):
         assert self.base_model.driver == self.driver_mock
         assert hasattr(self.base_model, "logger")
 
-    def test_verify_connection_success(self):
+    def test_verify_connection_should_success_with_working_driver(self):
         self.base_model.verify_connection()
         self.driver_mock.verify_connectivity.assert_called_once()
 
-    def test_verify_connection_failure(self):
+    def test_verify_connection_should_raise_exception_when_connection_error(self):
         self.driver_mock.verify_connectivity.side_effect = Exception("Connection error")
         with pytest.raises(Exception, match="Connection error"):
             self.base_model.verify_connection()
         self.driver_mock.verify_connectivity.assert_called_once()
 
-    def test_verify_connection_logger_debug(self):
+    def test_verify_connection_should_use_logger(self):
         with patch.object(self.base_model.logger, "debug") as logger_debug_mock:
             self.base_model.verify_connection()
             logger_debug_mock.assert_called_once_with(
                 "Connection verified", method=self.base_model.verify_connection.__name__
             )
 
-    def test_verify_connection_logger_error(self):
+    def test_verify_connection_should_log_errors(self):
         self.driver_mock.verify_connectivity.side_effect = Exception("Connection error")
         with patch.object(self.base_model.logger, "error") as logger_error_mock:
             with pytest.raises(Exception):
@@ -43,8 +51,145 @@ class TestBaseModel:
                 "Connection failed", method=self.base_model.verify_connection.__name__, error="Connection error"
             )
 
-    def test_verify_connection_invalid_driver(self):
+    def test_verify_connection_should_raise_exception_on_init_with_invalid_driver(self):
         invalid_driver = {}
-        base_model_invalid = BaseModel(invalid_driver)
+        base_model_invalid = BaseModelChild(self.driver_mock)
+        base_model_invalid.driver = invalid_driver
         with pytest.raises(AttributeError):
             base_model_invalid.verify_connection()
+
+
+class TestArixSet:
+
+    CS = "CS"
+    COMPUTER_SCIENCE = "Computer Science"
+    UUID = "1234-5678-9012-3456"
+    CREATED = "2021-01-01T00:00:00"
+    LAST_MODIFIED = "2021-01-01T00:00:00"
+
+    SINGLE_CREATE_RECORDS_RETURN = MagicMock(
+        data=lambda: {
+            "a": {
+                "code": TestArixSet.CS,
+                "name": TestArixSet.COMPUTER_SCIENCE,
+                "uuid": TestArixSet.UUID,
+                "created": TestArixSet.CREATED,
+                "last_modified": TestArixSet.LAST_MODIFIED,
+            }
+        }
+    )
+
+    @pytest.fixture
+    def code(self):
+        return TestArixSet.CS
+
+    @pytest.fixture
+    def driver(self):
+        driver = MagicMock(spec=Driver)
+        driver.verify_connectivity = MagicMock()
+        return driver
+
+    @pytest.fixture
+    def name(self):
+        return TestArixSet.COMPUTER_SCIENCE
+
+    @pytest.fixture
+    def uuid_fixture(self):
+        return TestArixSet.UUID
+
+    @pytest.fixture
+    def created_fixture(self):
+        return TestArixSet.CREATED
+
+    @pytest.fixture
+    def last_modified_fixture(self):
+        return TestArixSet.LAST_MODIFIED
+
+    def test_init_should_succeed_with_valid_params(self, driver, code, name):
+        arxiv_set = ArxivSet(driver, code, name)
+        assert arxiv_set.driver == driver
+        assert arxiv_set.code == code
+        assert arxiv_set.name == name
+        assert arxiv_set.uuid is None
+        assert arxiv_set.created is None
+        assert arxiv_set.last_modified is None
+
+    @pytest.mark.parametrize(
+        "d, c, n",
+        [
+            (None, "cs", "name"),
+            (123, "cs", "name"),
+            (MagicMock(), None, "name"),
+            (MagicMock(), 123, "name"),
+            (MagicMock(), "cs", None),
+            (MagicMock(), "cs", 123),
+        ],
+    )
+    def test_init_should_raise_exception_with_invalid_params(self, d, c, n):
+        with pytest.raises(ValueError):
+            ArxivSet(d, c, n)
+
+    @patch("services.services_layer.src.services_layer.models.uuid")
+    def test_create_should_succeed_with_valid_params(self, mock_uuid, driver, code, name, uuid_fixture):
+        mock_uuid.uuid4 = MagicMock(return_value=uuid_fixture)
+        driver.execute_query.return_value = (
+            [
+                MagicMock(
+                    data=lambda: {
+                        "a": {
+                            "code": self.CS,
+                            "name": self.COMPUTER_SCIENCE,
+                            "uuid": self.UUID,
+                            "created": self.CREATED,
+                            "last_modified": self.LAST_MODIFIED,
+                        }
+                    }
+                )
+            ],
+            MagicMock(counters=MagicMock(nodes_created=1)),
+            ["a"],
+        )
+        arxiv_set = ArxivSet(driver, code, name)
+        arxiv_set.create()
+        driver.execute_query.assert_called_once()
+        assert arxiv_set.uuid == self.UUID
+        assert arxiv_set.created
+        assert arxiv_set.last_modified
+        print(arxiv_set.created)
+
+    @pytest.mark.parametrize(
+        "d, c, n",
+        [
+            (None, "cs", "name"),
+            (123, "cs", "name"),
+            (MagicMock(), None, "name"),
+            (MagicMock(), 123, "name"),
+            (MagicMock(), "cs", None),
+            (MagicMock(), "cs", 123),
+        ])
+    def test_create_should_raise_exception_with_invalid_params(self, d, c, n, driver, code, name):
+        arxiv_set = ArxivSet(driver, code, name)
+        with pytest.raises(ValueError):
+            arxiv_set.driver = driver
+            arxiv_set.code = code
+            arxiv_set.name = name
+            arxiv_set.create()
+    
+    def test_create_should_raise_exception_when_no_records_returned(self, driver, code, name):
+        driver.execute_query.return_value = ([], MagicMock(counters=MagicMock(nodes_created=0)), [])
+        arxiv_set = ArxivSet(driver, code, name)
+        with pytest.raises(RuntimeError):
+            arxiv_set.create()
+        driver.execute_query.assert_called_once()
+    
+    def test_create_should_not_duplicate_arxivset(self, driver, code):
+        driver.execute_query.return_value = (
+            [self.SINGLE_CREATE_RECORDS_RETURN],
+            MagicMock(counters=MagicMock(nodes_created=0)),
+            ["a"],
+        )
+        name = "C0mput3r Sc13nc3"
+        arxiv_set = ArxivSet(driver, code, name)
+        arxiv_set.create()
+        driver.execute_query.assert_called_once()
+        assert arxiv_set.name == self.COMPUTER_SCIENCE
