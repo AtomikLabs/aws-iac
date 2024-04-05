7b9ee2504f7ebc4285042c904b86969d82a23747
Brad Edwards <j.bradley.edwards@gmail.com>
2024-04-04 21:06:22 -0700

    Update temp scripts

--- scripts/custom/keyword_search.py
+++ scripts/custom/keyword_search.py
@@ -11,7 +11,7 @@ from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
 from docx.shared import RGBColor
 
-target_date = datetime(2024, 3, 22)
+target_date = datetime(2024, 4, 3)
 
 
 def latex_to_human_readable(latex_str):
