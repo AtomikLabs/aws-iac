61eeb6f18a654298b70f849b17b1037a4393a579
Brad Edwards <j.bradley.edwards@gmail.com>
2024-04-24 13:19:38 -0700

    Add test DAG for AWS integration

---
+++ orchestration/airflow/dags/aws_services_test_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 4, 24),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'aws_services_test_dag',
+    default_args=default_args,
+    description='A simple DAG to test AWS CLI access',
+    schedule_interval=None,
+)
+
+test_aws_cli = BashOperator(
+    task_id='aws_services_test_dag_command',
+    bash_command='aws s3 ls s3://dev-atomiklabs-data-bucket/',
+    dag=dag,
+)
