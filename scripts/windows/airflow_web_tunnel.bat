616426ebb4aee7d94d9d4ac91dd2befaf851d375
Brad Edwards <j.bradley.edwards@gmail.com>
2024-04-23 22:31:12 -0700

    Reduce size of orchestration instance

--- scripts/windows/airflow_web_tunnel.bat
+++ scripts/windows/airflow_web_tunnel.bat
@@ -2,12 +2,12 @@
 setlocal
 
 if "%~2"=="" (
-    echo Usage: %0 PrivateIP BastionIP
+    echo Usage: %0 PrivateIP OrchestrationIP
     exit /b 1
 )
 
 set "PrivateIP=%~1"
-set "BastionIP=%~2"
+set "OrchestrationIP=%~2"
 
 set "sshKeyPath=%USERPROFILE%\.ssh\dev-atomiklabs-bastion-keypair.pem"
 set "sshUser=ec2-user"
@@ -17,7 +17,7 @@ if not exist "%sshKeyPath%" (
     exit /b 1
 )
 
-ssh -NfL localhost:7474:%PrivateIP%:7474 -L localhost:7687:%PrivateIP%:7687 -i "%sshKeyPath%" %sshUser%@%BastionIP%
-echo SSH tunnel established to %PrivateIP% via %BastionIP%.
+ssh -NfL localhost:8080:%PrivateIP%:8080 -i "%sshKeyPath%" %sshUser%@%OrchestrationIP%
+echo SSH tunnel established to %PrivateIP% via %OrchestrationIP%.
 
 endlocal
