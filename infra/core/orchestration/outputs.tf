a6f995614726ceaf098ab02b4c0a74bea61d1d13
Brad Edwards <j.bradley.edwards@gmail.com>
2024-04-27 12:43:01 -0700

    Add orchestration host has permissions to access neo4j

--- infra/core/orchestration/outputs.tf
+++ infra/core/orchestration/outputs.tf
@@ -2,3 +2,8 @@ output "airflow_instance_id" {
   description = "ID of the Airflow instance"
   value       = aws_instance.orchestration_host.id
 }
+
+output "orchestration_security_group_id" {
+  description = "ID of the security group for the orchestration host"
+  value       = aws_security_group.orchestration_security_group.id
+}
