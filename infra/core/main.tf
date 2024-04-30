a5147ded7735b8f56b7224060a34f05f89b6fad0
Brad Edwards <j.bradley.edwards@gmail.com>
2024-04-30 09:23:03 -0700

    Update module name for security group

--- infra/core/main.tf
+++ infra/core/main.tf
@@ -165,7 +165,7 @@ module "orchestration" {
                                                     ]
   private_subnets                                 = module.networking.aws_private_subnet_ids
   rabbitmq_source_security_group_ids              = [
-                                                      module.networking.neo4j_security_group_id,
+                                                      module.data_management.neo4j_security_group_id,
                                                       module.security.bastion_host_security_group_id,
                                                     ]
   region                                          = local.aws_region
