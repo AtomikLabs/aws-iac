4b40aac75286652a89fe800f146489f5d33c6c54
Brad Edwards <j.bradley.edwards@gmail.com>
2024-05-01 20:39:20 -0700

    Update schema compatibility for dev

--- infra/core/orchestration/main.tf
+++ infra/core/orchestration/main.tf
@@ -350,7 +350,7 @@ resource "aws_glue_registry" "glue_registry" {
 
 resource "aws_glue_schema" "arxiv_research_ingestion_event_schema" {
     schema_name = "${local.environment}-arxiv_research-ingestion-event-schema"
-    compatibility = "BACKWARD"
+    compatibility = "NONE"
     data_format = "AVRO"
     registry_arn = aws_glue_registry.glue_registry.arn
     schema_definition = file("${path.module}/schemas/arxiv_research_ingestion_event_schema.avsc")
