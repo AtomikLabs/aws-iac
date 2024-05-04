68577351b0500f6fc1b12fd15d5c72739b9ff59a
Brad Edwards <j.bradley.edwards@gmail.com>
2024-05-03 20:10:00 -0700

    Update script conditions

--- orchestration/airflow/host_config/sync_s3.sh
+++ orchestration/airflow/host_config/sync_s3.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-LOCK_FILE=/data/.docker_op_complete
-if [ -f $LOCK_FILE ]; then
-    aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/airflow /data/airflow --recursive
-    aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/airflow/dag/.env /data/airflow/dag/.env
-fi
+aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/airflow /data/airflow --recursive
+aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/airflow/dag/.env /data/airflow/dag/.env
--- orchestration/kafka/host_config/sync_s3.sh
+++ orchestration/kafka/host_config/sync_s3.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-LOCK_FILE=/data/.docker_op_complete
-if [ -f $LOCK_FILE ]; then
-    aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/kafka /data/kafka --recursive
-    aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/kafka/topics/.env /data/kafka/topics/.env
-fi
+aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/kafka /data/kafka --recursive
+aws s3 cp s3://$ATOMIKLABS_INFRA_CONFIG_BUCKET_NAME/orchestration/$ATOMIKLABS_ENV/kafka/topics/.env /data/kafka/topics/.env
