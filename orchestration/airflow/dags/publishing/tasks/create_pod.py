0a59b04b39d70d42b1c1f741ee86c932d0dc5bc3
Brad Edwards <j.bradley.edwards@gmail.com>
2024-05-20 08:58:41 -0700

    Get arxiv records by date and category required

--- orchestration/airflow/dags/publishing/tasks/create_pod.py
+++ orchestration/airflow/dags/publishing/tasks/create_pod.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from logging.config import dictConfig
 from typing import Dict, List
 
@@ -45,12 +45,22 @@ def run(
         logger.info("Creating pod", set=arxiv_set, category=category)
         env_vars = [AWS_REGION, ENVIRONMENT_NAME]
         config = get_config(context, env_vars, True)
-        next_date = last_pod_date(config, arxiv_set, category) + timedelta(days=1)
+        next_date = last_pod_date(config, arxiv_set, category)
+        if not next_date:
+            logger.info("Podcasts up to date", set=arxiv_set, category=category, method=last_pod_date.__name__)
+            return {"statusCode": 200, "body": "Podcasts up to date"}
+        next_date = next_date + timedelta(days=1)
         date_list = [next_date + timedelta(days=i) for i in range((datetime.now() - next_date).days)]
+        if not date_list:
+            logger.info("No dates require podcasts", set=arxiv_set, category=category, date_list=date_list)
+            return {"statusCode": 200, "body": "No dates require podcasts", "date_list": date_list}
         logger.info("Date list", date_list=date_list)
         for pod_date in date_list:
             logger.info("Creating pod", set=arxiv_set, category=category, date=pod_date)
             summaries = get_summaries(config, arxiv_set, category, pod_date)
+            if not summaries:
+                logger.info("No summaries for date", set=arxiv_set, category=category, date=pod_date)
+                continue
             pod_summaries = get_pod_summaries(summaries)
             pod_scripts = get_pod_scripts(pod_summaries, arxiv_set, category, pod_date)
             generate_pods(config, pod_scripts)
@@ -61,7 +71,7 @@ def run(
         return {"statusCode": 500, "body": "Error creating pod"}
 
 
-def last_pod_date(config: dict, arxiv_set: str, category: str) -> datetime:
+def last_pod_date(config: dict, arxiv_set: str, category: str) -> date:
     logger.info("Getting last pod date")
     try:
         driver = GraphDatabase.driver(config["NEO4J_URI"], auth=(config["NEO4J_USERNAME"], config["NEO4J_PASSWORD"]))
@@ -72,7 +82,8 @@ def last_pod_date(config: dict, arxiv_set: str, category: str) -> datetime:
                 "RETURN p.date ORDER BY p.date DESC LIMIT 1"
             )
             result = session.run(query, {"arxiv_set": arxiv_set, "category": category})
-            return result.data()[0]["p.date"]
+            data = result.data()
+            return data[0]["p.date"].to_native() if data else None
     except Exception as e:
         logger.error("Error getting last pod date", error=e)
         raise e
@@ -80,7 +91,23 @@ def last_pod_date(config: dict, arxiv_set: str, category: str) -> datetime:
 
 def get_summaries(config: dict, arxiv_set: str, category: str, episode_date: datetime) -> List[Dict[str, str]]:
     logger.info("Getting summaries", set=arxiv_set, category=category, date=episode_date)
-    pass
+    try:
+        driver = GraphDatabase.driver(config["NEO4J_URI"], auth=(config["NEO4J_USERNAME"], config["NEO4J_PASSWORD"]))
+        with driver.session() as session:
+            query = (
+                f"MATCH (c:ArxivCategory {{code: $category}})"
+                f"-[:{CATEGORIZES}]->(a:ArxivRecord) "
+                f"WHERE a.date = $date "
+                "RETURN a"
+            )
+            result = session.run(query, {"arxiv_set": arxiv_set, "category": category, "date": episode_date.date()})
+            data = result.data()
+            logger.info(data)
+            return [r["a"] for r in data]
+            return data
+    except Exception as e:
+        logger.error("Error getting summaries", error=e)
+        raise e
 
 
 def get_pod_summaries(summaries: List[Dict[str, str]]) -> List[Dict[str, str]]:
